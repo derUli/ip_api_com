8ef79e128071c9f73abea1786039ebe453d84ae0
Ulrich Schmidt <admin@deruli.de>
2017-10-28 13:34:59 +0200

    example fixed

--- example/src/content/modules/ip_api_com_example/IpApiComExample.php
+++ example/src/content/modules/ip_api_com_example/IpApiComExample.php
@@ -6,7 +6,7 @@ class IpApiComExample extends Controller
     public function render()
     {
         ob_start();
-        $controller = ModuleHelper::getMainController("IpApiCom");
+        $controller = ModuleHelper::getMainController("ip_api_com");
         var_dump($controller->getGeoLocation());
         return nl2br(htmlspecialchars(ob_get_clean()));
     }
